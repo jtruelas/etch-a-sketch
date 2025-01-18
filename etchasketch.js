d8af98bba98c5f804576aa60ff9db12dc8dc7e38
jtruelas <joshuatruelas@gmail.com>
2025-01-18 11:05:19 -0800

    Fixed issue with writing outside of and on etch of sketch border

--- etchasketch.js
+++ etchasketch.js
@@ -9,19 +9,46 @@ for(let i = 0; i < 16; i++) {
 }
 
 // Create event listener
-document.addEventListener('mousemove', (event) => {
+container.addEventListener('mousemove', (event) => {
+    // Create boundary for screen
+    const screen = container.getBoundingClientRect();
+
+    // Find the border size
+    const computedStyle = getComputedStyle(container);
+    const borderTop = parseInt(computedStyle.borderTopWidth, 10);
+    const borderBottom = parseInt(computedStyle.borderBottomWidth, 10);
+    const borderLeft = parseInt(computedStyle.borderLeftWidth, 10);
+    const borderRight = parseInt(computedStyle.borderRightWidth, 10);
+
+    // Find inner boundaries
+    const screenTop = screen.top + borderTop;
+    const screenBottom = screen.bottom - borderBottom - 5;
+    const screenLeft = screen.left + borderLeft;
+    const screenRight = screen.right - borderRight - 5;
+
+    console.log(`Screen Dimensions:\nTop: ${screenTop}\n Bottom: ${screenBottom}\n Left: ${screenLeft}\n Right: ${screenRight}\n`);
+    console.log(`Mouse Location:\nX: ${event.clientX}\nY: ${event.clientY}`);
+
+    // Check if mouse is on screen
+    if(event.clientX >= screenLeft &&
+        event.clientX < screenRight &&
+        event.clientY >= screenTop &&
+        event.clientY < screenBottom
+    ) {
     const sketch = document.createElement('div');
     sketch.style.position = 'absolute';
     sketch.style.width = '8px';
     sketch.style.height = '8px';
     sketch.style.borderRadius = '50%';
     sketch.style.backgroundColor = 'black';
-    sketch.style.left = event.clientX + 'px';
-    sketch.style.top = event.clientY + 'px';
+    sketch.style.top = `${event.clientY}px`;
+    sketch.style.left = `${event.clientX}px`;
     container.appendChild(sketch);
+    }
 });
 
 // Create button element with a prompt
 const button = document.createElement('button');
 button.setAttribute('id', 'grid-button');
+button.textContent = "Create New Grid";
 bodySelector.prepend(button);
