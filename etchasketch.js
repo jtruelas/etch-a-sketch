349eee919854aad2ca200f766188ada124468eaf
jtruelas <joshuatruelas@gmail.com>
2025-01-18 00:19:20 -0800

    Created first div

---
+++ etchasketch.js
@@ -0,0 +1,6 @@
+// Create base 16x16 structure
+const container = document.getElementById('container');
+const div = document.createElement('div');
+div.setAttribute('id', 'layout');
+div.textContent = 'Div';
+container.append(div);
