ec7b4606fd1471ae7636cc50bbc6539c355170d8
jtruelas <joshuatruelas@gmail.com>
2025-01-18 00:33:40 -0800

    Created a loop to create a 16x16 grid

--- etchasketch.js
+++ etchasketch.js
@@ -1,6 +1,9 @@
 // Create base 16x16 structure
 const container = document.getElementById('container');
-const div = document.createElement('div');
-div.setAttribute('id', 'layout');
-div.textContent = 'Div';
-container.append(div);
+
+for(let i = 0; i < 16; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('id', 'layout');
+    div.textContent = `Div${i}`;
+    container.append(div);
+}
