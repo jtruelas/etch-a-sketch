7f4fa218e37d947d918f60afef42df9291e078ab
jtruelas <joshuatruelas@gmail.com>
2025-01-18 09:16:48 -0800

    Added hover feature that leaves trail like a etch-a-sketch

--- etchasketch.js
+++ etchasketch.js
@@ -6,3 +6,16 @@ for(let i = 0; i < 16; i++) {
     div.setAttribute('id', 'layout');
     container.append(div);
 }
+
+// Create event listener
+document.addEventListener('mousemove', (event) => {
+    const sketch = document.createElement('div');
+    sketch.style.position = 'absolute';
+    sketch.style.width = '8px';
+    sketch.style.height = '8px';
+    sketch.style.borderRadius = '50%';
+    sketch.style.backgroundColor = 'black';
+    sketch.style.left = event.clientX + 'px';
+    sketch.style.top = event.clientY + 'px';
+    container.appendChild(sketch);
+});
